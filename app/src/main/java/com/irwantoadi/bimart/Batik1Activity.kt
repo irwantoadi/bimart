34060652276a65011975df448d01e454e1cf9d5a
irwantoadi <irwantoadi926@gmail.com>
2023-04-30 20:39:24 +0700

    project prototype aplikasi bimart tentang batik

---
+++ app/src/main/java/com/irwantoadi/bimart/Batik1Activity.kt
@@ -0,0 +1,11 @@
+package com.irwantoadi.bimart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Batik1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_batik1)
+    }
+}
