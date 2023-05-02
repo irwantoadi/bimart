52a73050796e67ce1fa10a3b286753c05d3ebae6
irwantoadi <irwantoadi926@gmail.com>
2023-05-02 22:04:14 +0700

    update layout, add drawable, add logic activity

--- app/src/main/java/com/irwantoadi/bimart/LoginActivity.kt
+++ app/src/main/java/com/irwantoadi/bimart/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.irwantoadi.bimart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val btnLogin: Button = findViewById(R.id.btn_login)
+        val btnRegister: Button = findViewById(R.id.btn_register)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        btnRegister.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
